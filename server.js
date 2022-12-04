27e676640ac7acc1f36641e9d50c1b516bd0b113
Mike <mskowron4@yahoo.com>
2022-12-03 17:56:06 -0800

    view employee function

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const express = require('express');
+const { query } = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express(); 
@@ -61,6 +62,35 @@ function promptOne() {
         }
     });
 
+}
+
+function viewEmployee() {
+    console.log('You are now viewing employees.');
+
+    var sql = `
+    SELECT employee.id,
+     employee.first_name,
+      employee.last_name,
+       roles.title,
+        department.name AS department,
+         roles.salary,
+         CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+         FROM employee
+         LEFT JOIN roles
+         ON employee.roles_id = roles.id
+         LEFT JOIN department 
+         ON department.id = roles.department_id
+         LEFT JOIN employee 
+         ON manager.id = employee.manager_id`
+
+         db.sql(query, function (err, res) {
+            if (err) throw err;
+
+            console.table(res);
+            console.log ('You are now viewing employees');
+
+            promptOne();
+         })
 }
 
 
